8cb8fefbdc784e69b4dd34c1e6a726b1f286eefb
Santosh Sarkar <sarkar.santosh334@gmail.com>
2026-03-02 18:17:51 +0545

    email service chalja sim sim

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -9,8 +9,8 @@ try {
 // Create transporter - configure with your email provider
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,        
-  secure: false,  
+  port: 465,        
+  secure: true,  
   family: 4,        
   auth: {
     user: process.env.EMAIL_USER,
