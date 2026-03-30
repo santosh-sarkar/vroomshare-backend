0e37257966c18c584e75a6944bea1cd5ec588a13
Santosh Sarkar <sarkar.santosh334@gmail.com>
2026-03-30 21:42:55 +0545

    Fix vehicle creation: set default description

--- src/controllers/vehicle.controller.js
+++ src/controllers/vehicle.controller.js
@@ -131,7 +131,7 @@ async function create(req, res, next) {
       year,
       engineCc,
       registrationNumber,
-      description,
+      description: description || "description",
       features: features || [],
 
       pickup: {
