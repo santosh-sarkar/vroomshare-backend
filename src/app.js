d7f793d7be47ee343b5cf21ae1ed0921a0112e59
Santosh Sarkar <sarkar.santosh334@gmail.com>
2026-08-25 12:57:28 +0545

    fix(cors): add additional allowed origin for CORS configuration

--- src/app.js
+++ src/app.js
@@ -10,7 +10,8 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:3000",
   "https://vroomshare-frontend.vercel.app",
-  "https://vroomshare.vercel.app"
+  "https://vroomshare.vercel.app",
+  "https://vroomshare.santoshsarkar.com.np/"
 ];
 app.use(cors({ 
   origin: allowedOrigins, 
